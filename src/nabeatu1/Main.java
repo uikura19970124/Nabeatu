ffcfcb7e444be46eb98ad3e4f8daf554c0eb7115
iTAP <iTAP@iTAP-PC12>
2015-05-18 16:22:37 +0900

    Mathクラスを作成

--- src/nabeatu1/Main.java
+++ src/nabeatu1/Main.java
@@ -3,8 +3,18 @@ package nabeatu1;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Math math = new Math();
 
+		for(int i=1;i<=40;i++){
+			if (i%3==0 || math.Search(i) == 0 ){
+				System.out.println("Aho");
+			}else {
+				System.out.println(i);
+			}
+		}
 	}
 
+
 }
+
+
---
+++ src/nabeatu1/Math.java
@@ -0,0 +1,16 @@
+package nabeatu1;
+
+public class Math {
+	public static int Search (int num){
+		String str = String.valueOf(num);
+		if( str.indexOf('3') !=-1){
+			return 0;
+		}else
+			return -1;
+	}
+
+
+
+}
+
+
